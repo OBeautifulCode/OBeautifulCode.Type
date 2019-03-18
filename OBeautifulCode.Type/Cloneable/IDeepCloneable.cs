4342381188965dd8baa2211c2b9cb753ea309b40
Suraj Gupta <SurajGupta@users.noreply.github.com>
2019-03-18 11:31:36 -0400

    added IDeepCloneable and IShallowCloneable

---
+++ OBeautifulCode.Type/Cloneable/IDeepCloneable.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDeepCloneable.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+
+    /// <summary>
+    /// Supports deep cloning, which creates a new instance of a class that is equal to
+    /// a reference instance, but where the two instances do not share any memory between
+    /// their full object graphs.
+    /// </summary>
+    /// <typeparam name="T">The type of object to clone.</typeparam>
+    public interface IDeepCloneable<T> : ICloneable
+    {
+        /// <summary>
+        /// Creates a new object that is a deep clone of this instance.
+        /// </summary>
+        /// <returns>
+        /// A new object that is a deep clone of this instance.
+        /// </returns>
+        T DeepClone();
+    }
+}
---
+++ OBeautifulCode.Type/Cloneable/IShallowCloneable.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IShallowCloneable.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+
+    /// <summary>
+    /// Supports shallow cloning, which creates a new instance of a class that is equal to
+    /// a reference instance, but where the two instances share memory somewhere in their
+    /// object graphs.
+    /// </summary>
+    /// <typeparam name="T">The type of object to clone.</typeparam>
+    public interface IShallowCloneable<T> : ICloneable
+    {
+        /// <summary>
+        /// Creates a new object that is a shallow clone of this instance.
+        /// </summary>
+        /// <returns>
+        /// A new object that is a shallow clone of this instance.
+        /// </returns>
+        T DeepClone();
+    }
+}
