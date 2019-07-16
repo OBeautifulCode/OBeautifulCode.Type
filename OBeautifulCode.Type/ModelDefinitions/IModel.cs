29b8a593ed64a57fa704b8e1178555a08abc05fa
Lawson Caudill <lawson@getthinktank.com>
2019-07-16 15:15:26 -0400

    Added IModel<T> interface to consolidate interfaces and contract of model objects.

---
+++ OBeautifulCode.Type/ModelDefinitions/IModel.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IModel.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+
+    /// <summary>
+    /// Represents the contract of a model object.
+    /// </summary>
+    /// <typeparam name="T">The type of object being modeled.</typeparam>
+    public interface IModel<T> : IEquatable<T>, IDeepCloneable<T>
+    {
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
+        int GetHashCode();
+
+        /// <summary>
+        /// Construct a friendly string representation of this object.
+        /// </summary>
+        /// <returns>A <see cref="string" /> that represents this instance.</returns>
+        string ToString();
+    }
+}
