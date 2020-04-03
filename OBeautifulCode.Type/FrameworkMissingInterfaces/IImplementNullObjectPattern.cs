dc0cac0408af8d580ccf9bc3fbfeafc3233b22bb
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-04-03 13:27:50 -0400

    added IImplementNullObjectPattern

---
+++ OBeautifulCode.Type/FrameworkMissingInterfaces/IImplementNullObjectPattern.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IImplementNullObjectPattern.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.Type.Internal;
+
+    /// <summary>
+    /// Interface to express usage of the Null Object Pattern (like an empty implementation of the contract it honors).
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = ObcSuppressBecause.CA1040_AvoidEmptyInterfaces_NeedToIdentifyGroupOfTypesAndPreferInterfaceOverAttribute)]
+    public interface IImplementNullObjectPattern
+    {
+    }
+}
