b2407072aad9b64a5fb2b1b2d93eefa2a7f1c749
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-04-09 12:16:31 -0400

    add A.Dummy<Type> to dummy factory
    gets a real-looking random type from loaded types

--- OBeautifulCode.Type.Test/TypeDummyFactory.cs
+++ OBeautifulCode.Type.Test/TypeDummyFactory.cs
@@ -10,10 +10,13 @@
 namespace OBeautifulCode.Type.Test
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     using FakeItEasy;
 
     using OBeautifulCode.AutoFakeItEasy;
+    using OBeautifulCode.Math.Recipes;
 
     /// <summary>
     /// A Dummy Factory for types in <see cref="OBeautifulCode.Type"/>.
@@ -24,6 +27,26 @@ namespace OBeautifulCode.Type.Test
 #endif
     public class TypeDummyFactory : DefaultTypeDummyFactory
     {
+        private static readonly IReadOnlyList<Type> LoadedTypes =
+            AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .Where(a => (a != null) && (!a.IsDynamic))
+                .SelectMany(_ => _.GetTypes())
+                .Where(_ => _ != null)
+                .Where(_ => !string.IsNullOrWhiteSpace(_.Namespace))
+                .ToList();
+
+        private static readonly IReadOnlyList<Type> AppDomainClosedTypes =
+            LoadedTypes
+                .Where(_ => !_.ContainsGenericParameters)
+                .ToList();
+
+        private static readonly IReadOnlyList<Type> AppDomainGenericTypeDefinitions =
+            LoadedTypes
+                .Where(_ => _.IsGenericTypeDefinition)
+                .ToList();
+
         public TypeDummyFactory()
         {
             AutoFixtureBackedDummyFactory.AddDummyCreator(() =>
@@ -36,6 +59,56 @@ namespace OBeautifulCode.Type.Test
 
                 return result;
             });
+
+            AutoFixtureBackedDummyFactory.AddDummyCreator(
+                () =>
+                {
+                    Type result;
+
+                    if (ThreadSafeRandom.Next(0, 2) == 0)
+                    {
+                        result = GetRandomClosedTypeInAppDomain();
+                    }
+                    else
+                    {
+                        while (true)
+                        {
+                            var genericTypeDefinition = GetRandomGenericTypeDefinitionInAppDomain();
+
+                            var typeArguments = Enumerable.Range(0, genericTypeDefinition.GetGenericArguments().Length).Select(_ => GetRandomClosedTypeInAppDomain()).ToArray();
+
+                            try
+                            {
+                                result = genericTypeDefinition.MakeGenericType(typeArguments);
+
+                                break;
+                            }
+                            catch (Exception)
+                            {
+                            }
+                        }
+                    }
+
+                    return result;
+                });
+        }
+
+        private Type GetRandomClosedTypeInAppDomain()
+        {
+            var randomIndex = ThreadSafeRandom.Next(0, AppDomainClosedTypes.Count);
+
+            var result = AppDomainClosedTypes[randomIndex];
+
+            return result;
+        }
+
+        private Type GetRandomGenericTypeDefinitionInAppDomain()
+        {
+            var randomIndex = ThreadSafeRandom.Next(0, AppDomainGenericTypeDefinitions.Count);
+
+            var result = AppDomainGenericTypeDefinitions[randomIndex];
+
+            return result;
         }
     }
 }
