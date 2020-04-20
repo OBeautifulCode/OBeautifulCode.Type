595deb35f51e260948a807a4a9edf0154f7e3488
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-04-20 15:53:42 -0400

    added IHaveTags

---
+++ OBeautifulCode.Type/FrameworkMissingInterfaces/IHaveTags.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IHaveTags.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents an object that can be tagged using key/value pairs of strings.
+    /// </summary>
+    public interface IHaveTags
+    {
+        /// <summary>
+        /// Gets or sets tags as key/value pairs of strings.
+        /// </summary>
+        /// <remarks>
+        /// null is valid value.  If the tags have a single dimension/no grouping concept,
+        /// then they can be specified as the keys with entirely null values.
+        /// </remarks>
+        IReadOnlyDictionary<string, string> Tags { get; set; }
+    }
+}
