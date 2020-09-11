02153fb6a6eb51bf3f7af4cc65867fbe73536d31
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-09-11 15:06:16 -0400

    added IHaveTimestampUtc

---
+++ OBeautifulCode.Type/FrameworkMissingInterfaces/IHaveTimestampUtc.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IHaveTimestampUtc.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+
+    /// <summary>
+    /// Represents an object that is timestamped in UTC.
+    /// </summary>
+    public interface IHaveTimestampUtc
+    {
+        /// <summary>
+        /// Gets the timestamp in UTC.
+        /// </summary>
+        DateTime TimestampUtc { get; }
+    }
+}
