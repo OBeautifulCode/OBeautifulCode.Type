a20c4fe8747df7083650da6ee8dcd6e653a67981
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-10-02 10:55:46 -0400

    added IForsakeInheritedModelViaCodeGen

---
+++ OBeautifulCode.Type/ViaCodeGen/IForsakeInheritedModelViaCodeGen.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IForsakeInheritedModelViaCodeGen.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Used on a type to indicate that an inherited <see cref="IModelViaCodeGen"/> should be forsaken (don't generate code).
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = ObcSuppressBecause.CA1040_AvoidEmptyInterfaces_NeedToIdentifyGroupOfTypesAndPreferInterfaceOverAttribute)]
+
+    // ReSharper disable once UnusedMember.Global
+    public interface IForsakeInheritedModelViaCodeGen
+    {
+    }
+}
