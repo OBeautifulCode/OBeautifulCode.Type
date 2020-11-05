1313fd4bee61fbdd1dcf3d2a4e72d14a41b378bb
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-11-05 11:54:22 -0500

    added IForesakeDeepCloneWithViaCodeGen

---
+++ OBeautifulCode.Type/ViaCodeGen/IForsakeDeepCloneWithViaCodeGen.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IForsakeDeepCloneWithViaCodeGen.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Used on a type to indicate that DeepCloneWith... methods should be forsaken (don't generate code).
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = ObcSuppressBecause.CA1040_AvoidEmptyInterfaces_NeedToIdentifyGroupOfTypesAndPreferInterfaceOverAttribute)]
+
+    // ReSharper disable once UnusedMember.Global
+    public interface IForsakeDeepCloneWithViaCodeGen
+    {
+    }
+}
