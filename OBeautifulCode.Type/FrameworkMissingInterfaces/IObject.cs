df015c692142b5f510caaab476530c93a210ec93
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-01-29 19:19:01 -0500

    added IObject

--- OBeautifulCode.Type/FrameworkMissingInterfaces/IObject.cs
+++ OBeautifulCode.Type/FrameworkMissingInterfaces/IObject.cs
@@ -1,22 +1,27 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IModel.cs" company="OBeautifulCode">
+// <copyright file="IObject.cs" company="OBeautifulCode">
 //   Copyright (c) OBeautifulCode 2018. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace OBeautifulCode.Type
 {
-    using System;
     using System.Diagnostics.CodeAnalysis;
 
     using OBeautifulCode.CodeAnalysis.Recipes;
 
     /// <summary>
-    /// Represents the contract of a model object.
+    /// An interface alternative to <see cref="object"/>.
+    /// Enables a method (typically a constructor) to take-in an object of any type.
     /// </summary>
-    // ReSharper disable once UnusedMember.Global
+    /// <remarks>
+    /// This is particularly useful in providing a facility for a caller to construct
+    /// your object with their domain-specific context and then serialize your object.
+    /// Serializing an object having a property of type <see cref="object"/> can
+    /// break some serializers.
+    /// </remarks>
     [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = ObcSuppressBecause.CA1040_AvoidEmptyInterfaces_NeedToIdentifyGroupOfTypesAndPreferInterfaceOverAttribute)]
-    public interface IModel : IObject, IStringRepresentable, IHashable, ICloneable
+    public interface IObject
     {
     }
 }
