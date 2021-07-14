fdf5de719ad3312f2e9dc6ee3ab785f39f5af5c9
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-07-14 14:01:02 -0400

    added LambdaVoid and LambdaReturningProtocol

---
+++ OBeautifulCode.Type/Logic/Protocols/Concrete/LambdaReturningProtocol{TOperation,TReturn}.cs
@@ -0,0 +1,101 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LambdaReturningProtocol{TOperation,TReturn}.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using OBeautifulCode.Execution.Recipes;
+
+    /// <summary>
+    /// Protocolizes a returning lambda.
+    /// </summary>
+    /// <typeparam name="TOperation">Type of operation.</typeparam>
+    /// <typeparam name="TReturn">Type of the return.</typeparam>
+    public class LambdaReturningProtocol<TOperation, TReturn> : ISyncAndAsyncReturningProtocol<TOperation, TReturn>
+    where TOperation : IReturningOperation<TReturn>
+    {
+        private readonly Func<TOperation, TReturn> synchronousLambda;
+
+        private readonly Func<TOperation, Task<TReturn>> asyncAsynchronousLambda;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaReturningProtocol{TOperation,TReturn}"/> class.
+        /// </summary>
+        /// <param name="asynchronousLambda">The lambda to protocol the operation.</param>
+        public LambdaReturningProtocol(
+            Func<TOperation, Task<TReturn>> asynchronousLambda)
+        {
+            if (asynchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(asynchronousLambda));
+            }
+
+            this.asyncAsynchronousLambda = asynchronousLambda;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaReturningProtocol{TOperation,TReturn}"/> class.
+        /// </summary>
+        /// <param name="synchronousLambda">The lambda to protocol the operation.</param>
+        public LambdaReturningProtocol(
+            Func<TOperation, TReturn> synchronousLambda)
+        {
+            if (synchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(synchronousLambda));
+            }
+
+            this.synchronousLambda = synchronousLambda;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaReturningProtocol{TOperation,TReturn}"/> class.
+        /// </summary>
+        /// <param name="synchronousLambda">The synchronous lambda to protocol the operation.</param>
+        /// <param name="asynchronousLambda">The asynchronous lambda to protocol the operation.</param>
+        public LambdaReturningProtocol(
+            Func<TOperation, TReturn> synchronousLambda,
+            Func<TOperation, Task<TReturn>> asynchronousLambda)
+        {
+            if (synchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(synchronousLambda));
+            }
+
+            if (asynchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(asynchronousLambda));
+            }
+
+            this.synchronousLambda = synchronousLambda;
+            this.asyncAsynchronousLambda = asynchronousLambda;
+        }
+
+        /// <inheritdoc />
+        public TReturn Execute(
+            TOperation operation)
+        {
+            var result = this.synchronousLambda != null
+                ? this.synchronousLambda(operation)
+                : this.asyncAsynchronousLambda(operation).RunUntilCompletion();
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<TReturn> ExecuteAsync(
+            TOperation operation)
+        {
+            var result = await (this.asyncAsynchronousLambda != null
+                ? this.asyncAsynchronousLambda(operation)
+                : Task.FromResult(this.synchronousLambda(operation)));
+
+            return result;
+        }
+    }
+}
---
+++ OBeautifulCode.Type/Logic/Protocols/Concrete/LambdaVoidProtocol.cs
@@ -0,0 +1,106 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LambdaVoidProtocol.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using OBeautifulCode.Execution.Recipes;
+
+    /// <summary>
+    /// Protocolizes a non-returning lambda.
+    /// </summary>
+    /// <typeparam name="TOperation">Type of operation.</typeparam>
+    public class LambdaVoidProtocol<TOperation> : ISyncAndAsyncVoidProtocol<TOperation>
+    where TOperation : IVoidOperation
+    {
+        private readonly Action<TOperation> synchronousLambda;
+
+        private readonly Func<TOperation, Task> asyncAsynchronousLambda;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaVoidProtocol{TOperation}"/> class.
+        /// </summary>
+        /// <param name="asynchronousLambda">The lambda to protocol the operation.</param>
+        public LambdaVoidProtocol(
+            Func<TOperation, Task> asynchronousLambda)
+        {
+            if (asynchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(asynchronousLambda));
+            }
+
+            this.asyncAsynchronousLambda = asynchronousLambda;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaVoidProtocol{TOperation}"/> class.
+        /// </summary>
+        /// <param name="synchronousLambda">The lambda to protocol the operation.</param>
+        public LambdaVoidProtocol(
+            Action<TOperation> synchronousLambda)
+        {
+            if (synchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(synchronousLambda));
+            }
+
+            this.synchronousLambda = synchronousLambda;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaVoidProtocol{TOperation}"/> class.
+        /// </summary>
+        /// <param name="synchronousLambda">The synchronous lambda to protocol the operation.</param>
+        /// <param name="asynchronousLambda">The asynchronous lambda to protocol the operation.</param>
+        public LambdaVoidProtocol(
+            Action<TOperation> synchronousLambda,
+            Func<TOperation, Task> asynchronousLambda)
+        {
+            if (synchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(synchronousLambda));
+            }
+
+            if (asynchronousLambda == null)
+            {
+                throw new ArgumentNullException(nameof(asynchronousLambda));
+            }
+
+            this.synchronousLambda = synchronousLambda;
+            this.asyncAsynchronousLambda = asynchronousLambda;
+        }
+
+        /// <inheritdoc />
+        public void Execute(
+            TOperation operation)
+        {
+            if (this.synchronousLambda != null)
+            {
+                this.synchronousLambda(operation);
+            }
+            else
+            {
+                this.asyncAsynchronousLambda(operation).RunUntilCompletion();
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task ExecuteAsync(
+            TOperation operation)
+        {
+            if (this.asyncAsynchronousLambda != null)
+            {
+                await this.asyncAsynchronousLambda(operation);
+            }
+            else
+            {
+                await Task.Run(() => this.synchronousLambda(operation));
+            }
+        }
+    }
+}
