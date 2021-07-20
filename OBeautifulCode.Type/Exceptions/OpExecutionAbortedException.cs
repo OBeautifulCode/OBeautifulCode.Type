4beec19057966d2bb718af1c5c11e42b818ee99e
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-07-20 15:24:05 -0400

    added OpExecutionAbortedException and base class

---
+++ OBeautifulCode.Type/Exceptions/OpExecutionAbortedException.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpExecutionAbortedException.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Exception thrown when the execution of an <see cref="IOperation"/> is aborted.
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors", Justification = ObcSuppressBecause.CA1032_ImplementStandardExceptionConstructors_ExceptionUsedInternallyAndConstructorsEnsureRequiredInfoAvailableWhenCaught)]
+    public class OpExecutionAbortedException : OpExecutionAbortedExceptionBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionAbortedException"/> class.
+        /// </summary>
+        /// <param name="operation">The operation that aborted the execution.</param>
+        public OpExecutionAbortedException(
+            IOperation operation)
+            : base(operation)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionAbortedException"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="operation">The operation that aborted the execution.</param>
+        public OpExecutionAbortedException(
+            string message,
+            IOperation operation)
+            : base(message, operation)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionAbortedException"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="innerException">Inner exception.</param>
+        /// <param name="operation">The operation that aborted the execution.</param>
+        public OpExecutionAbortedException(
+            string message,
+            Exception innerException,
+            IOperation operation)
+            : base(message, innerException, operation)
+        {
+        }
+    }
+}
---
+++ OBeautifulCode.Type/Exceptions/OpExecutionAbortedExceptionBase.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpExecutionAbortedExceptionBase.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Base class for exception thrown when the execution of an <see cref="IOperation"/> is aborted.
+    /// </summary>
+    [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = ObcSuppressBecause.CA1710_IdentifiersShouldHaveCorrectSuffix_ExceptionBaseClassShouldEndWithBase)]
+    [SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Justification = ObcSuppressBecause.CA2237_MarkISerializableTypesWithSerializable_ExceptionOnlyUsedInternallyAndWillNeverBeSerialized)]
+    public abstract class OpExecutionAbortedExceptionBase : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionAbortedExceptionBase"/> class.
+        /// </summary>
+        /// <param name="operation">The operation that aborted the execution.</param>
+        protected OpExecutionAbortedExceptionBase(
+            IOperation operation)
+            : base()
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionAbortedExceptionBase"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="operation">The operation that aborted the execution.</param>
+        protected OpExecutionAbortedExceptionBase(
+            string message,
+            IOperation operation)
+            : base(message)
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionAbortedExceptionBase"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="innerException">Inner exception.</param>
+        /// <param name="operation">The operation that aborted the execution.</param>
+        protected OpExecutionAbortedExceptionBase(
+            string message,
+            Exception innerException,
+            IOperation operation)
+            : base(message, innerException)
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Gets the operation that aborted the execution.
+        /// </summary>
+        public IOperation Operation { get; private set; }
+    }
+}
