de4d5edd6fa1c1b6d2b807dff88fb142364e3131
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-07-21 10:45:31 -0400

    added OpExecutionFailedException

---
+++ OBeautifulCode.Type/Exceptions/OpExecutionFailedException.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpExecutionFailedException.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Exception thrown when the execution of an <see cref="IOperation"/> has failed.
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors", Justification = ObcSuppressBecause.CA1032_ImplementStandardExceptionConstructors_ExceptionUsedInternallyAndConstructorsEnsureRequiredInfoAvailableWhenCaught)]
+    public class OpExecutionFailedException : OpExecutionFailedExceptionBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionFailedException"/> class.
+        /// </summary>
+        /// <param name="operation">OPTIONAL operation whose execution failed.  DEFAULT is to omit that operation.</param>
+        public OpExecutionFailedException(
+            IOperation operation = null)
+            : base(operation)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionFailedException"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="operation">OPTIONAL operation whose execution failed.  DEFAULT is to omit that operation.</param>
+        public OpExecutionFailedException(
+            string message,
+            IOperation operation = null)
+            : base(message, operation)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionFailedException"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="innerException">Inner exception.</param>
+        /// <param name="operation">OPTIONAL operation whose execution failed.  DEFAULT is to omit that operation.</param>
+        public OpExecutionFailedException(
+            string message,
+            Exception innerException,
+            IOperation operation = null)
+            : base(message, innerException, operation)
+        {
+        }
+    }
+}
---
+++ OBeautifulCode.Type/Exceptions/OpExecutionFailedExceptionBase.cs
@@ -0,0 +1,80 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpExecutionFailedExceptionBase.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Base class for exception thrown when the execution of an <see cref="IOperation"/> has failed.
+    /// </summary>
+    [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = ObcSuppressBecause.CA1710_IdentifiersShouldHaveCorrectSuffix_ExceptionBaseClassShouldEndWithBase)]
+    [SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Justification = ObcSuppressBecause.CA2237_MarkISerializableTypesWithSerializable_ExceptionOnlyUsedInternallyAndWillNeverBeSerialized)]
+    public abstract class OpExecutionFailedExceptionBase : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionFailedExceptionBase"/> class.
+        /// </summary>
+        /// <param name="operation">The operation whose execution failed.</param>
+        protected OpExecutionFailedExceptionBase(
+            IOperation operation)
+            : base()
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionFailedExceptionBase"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="operation">The operation whose execution failed.</param>
+        protected OpExecutionFailedExceptionBase(
+            string message,
+            IOperation operation)
+            : base(message)
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionFailedExceptionBase"/> class.
+        /// </summary>
+        /// <param name="message">Message for exception.</param>
+        /// <param name="innerException">Inner exception.</param>
+        /// <param name="operation">The operation whose execution failed.</param>
+        protected OpExecutionFailedExceptionBase(
+            string message,
+            Exception innerException,
+            IOperation operation)
+            : base(message, innerException)
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Gets the operation that was executing and is now failed.
+        /// </summary>
+        public IOperation Operation { get; private set; }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var result = string.Empty;
+
+            if (this.Operation != null)
+            {
+                result = result + "This operation's execution failed: " + this.Operation + Environment.NewLine;
+            }
+
+            result = result + base.ToString();
+
+            return result;
+        }
+    }
+}
