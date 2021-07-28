f412c6f77ed4fd7b1bc6abfb8428c939410bcefc
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-07-28 11:23:40 -0400

    added OpExecutionDeemedNotApplicableException

---
+++ OBeautifulCode.Type/Exceptions/OpExecutionDeemedNotApplicableException.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpExecutionDeemedNotApplicableException.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Exception thrown when the execution of an <see cref="IOperation"/> deems itself not applicable.
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors", Justification = ObcSuppressBecause.CA1032_ImplementStandardExceptionConstructors_ExceptionUsedInternallyAndConstructorsEnsureRequiredInfoAvailableWhenCaught)]
+    public class OpExecutionDeemedNotApplicableException : OpExecutionDeemedNotApplicableExceptionBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionDeemedNotApplicableException"/> class.
+        /// </summary>
+        /// <param name="operation">OPTIONAL operation whose execution is not applicable.  DEFAULT is to omit that operation.</param>
+        public OpExecutionDeemedNotApplicableException(
+            IOperation operation = null)
+            : base(operation)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionDeemedNotApplicableException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="operation">OPTIONAL operation whose execution is not applicable.  DEFAULT is to omit that operation.</param>
+        public OpExecutionDeemedNotApplicableException(
+            string message,
+            IOperation operation = null)
+            : base(message, operation)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionDeemedNotApplicableException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        /// <param name="operation">OPTIONAL operation whose execution is not applicable.  DEFAULT is to omit that operation.</param>
+        public OpExecutionDeemedNotApplicableException(
+            string message,
+            Exception innerException,
+            IOperation operation = null)
+            : base(message, innerException, operation)
+        {
+        }
+    }
+}
---
+++ OBeautifulCode.Type/Exceptions/OpExecutionDeemedNotApplicableExceptionBase.cs
@@ -0,0 +1,80 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpExecutionDeemedNotApplicableExceptionBase.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// Base class for exception thrown when the execution of an <see cref="IOperation"/> deems itself not applicable.
+    /// </summary>
+    [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = ObcSuppressBecause.CA1710_IdentifiersShouldHaveCorrectSuffix_ExceptionBaseClassShouldEndWithBase)]
+    [SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Justification = ObcSuppressBecause.CA2237_MarkISerializableTypesWithSerializable_ExceptionOnlyUsedInternallyAndWillNeverBeSerialized)]
+    public abstract class OpExecutionDeemedNotApplicableExceptionBase : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionDeemedNotApplicableExceptionBase"/> class.
+        /// </summary>
+        /// <param name="operation">The operation whose execution is not applicable.</param>
+        protected OpExecutionDeemedNotApplicableExceptionBase(
+            IOperation operation)
+            : base()
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionDeemedNotApplicableExceptionBase"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="operation">The operation whose execution is not applicable.</param>
+        protected OpExecutionDeemedNotApplicableExceptionBase(
+            string message,
+            IOperation operation)
+            : base(message)
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpExecutionDeemedNotApplicableExceptionBase"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        /// <param name="operation">The operation whose execution is not applicable.</param>
+        protected OpExecutionDeemedNotApplicableExceptionBase(
+            string message,
+            Exception innerException,
+            IOperation operation)
+            : base(message, innerException)
+        {
+            this.Operation = operation;
+        }
+
+        /// <summary>
+        /// Gets the operation whose execution is not applicable.
+        /// </summary>
+        public IOperation Operation { get; private set; }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var result = string.Empty;
+
+            if (this.Operation != null)
+            {
+                result = result + "This operation's execution was deemed not applicable: " + this.Operation + Environment.NewLine;
+            }
+
+            result = result + base.ToString();
+
+            return result;
+        }
+    }
+}
