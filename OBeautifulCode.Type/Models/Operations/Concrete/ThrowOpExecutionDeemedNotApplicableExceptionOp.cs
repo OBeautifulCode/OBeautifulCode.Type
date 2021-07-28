a2c65c0c2788a6c956f9924570dc0138c6006295
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-07-28 11:58:54 -0400

    added ThrowOpExecutionDeemedNotApplicableExceptionOp

---
+++ OBeautifulCode.Type/Models/Operations/Concrete/ThrowOpExecutionDeemedNotApplicableExceptionOp.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ThrowOpExecutionDeemedNotApplicableExceptionOp.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    /// <summary>
+    /// Operation that throws a <see cref="OpExecutionAbortedExceptionBase"/>.
+    /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class ThrowOpExecutionDeemedNotApplicableExceptionOp : VoidOperationBase, IThrowOpExecutionDeemedNotApplicableExceptionOp, IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThrowOpExecutionDeemedNotApplicableExceptionOp"/> class.
+        /// </summary>
+        /// <param name="details">Details about the operation.</param>
+        public ThrowOpExecutionDeemedNotApplicableExceptionOp(
+            string details)
+        {
+            this.Details = details;
+        }
+
+        /// <inheritdoc />
+        public string Details { get; private set; }
+    }
+}
---
+++ OBeautifulCode.Type/Models/Operations/Concrete/ThrowOpExecutionDeemedNotApplicableExceptionOp{TResult}.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ThrowOpExecutionDeemedNotApplicableExceptionOp{TResult}.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    /// <summary>
+    /// Operation that throws a <see cref="OpExecutionAbortedExceptionBase"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type returned when the operation is executed.</typeparam>
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class ThrowOpExecutionDeemedNotApplicableExceptionOp<TResult> : ReturningOperationBase<TResult>, IThrowOpExecutionDeemedNotApplicableExceptionOp, IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThrowOpExecutionDeemedNotApplicableExceptionOp{TResult}"/> class.
+        /// </summary>
+        /// <param name="details">Details about the operation.</param>
+        public ThrowOpExecutionDeemedNotApplicableExceptionOp(
+            string details)
+        {
+            this.Details = details;
+        }
+
+        /// <inheritdoc />
+        public string Details { get; private set; }
+    }
+}
