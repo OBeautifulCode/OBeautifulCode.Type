06418bbb40b38181e2b55ca3ccbd9ebe171706d0
Suraj Gupta <SurajGupta@users.noreply.github.com>
2023-04-19 11:12:33 -0400

    added IHaveName, pre-commit

---
+++ OBeautifulCode.Type/Interfaces/Property/IHaveName.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IHaveName.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    /// <summary>
+    /// Interface to declare having a name.
+    /// </summary>
+    public interface IHaveName
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        string Name { get; }
+    }
+}
