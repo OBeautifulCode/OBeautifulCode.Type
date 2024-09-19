524669755f80ceac9f311c61e1c078d08a412530
Suraj Gupta <SurajGupta@users.noreply.github.com>
2024-09-19 10:25:43 -0400

    added TypeSelectionStrategy

---
+++ OBeautifulCode.Type/Enums/TypeSelectionStrategy.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeSelectionStrategy.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    /// <summary>
+    /// Specifies the strategy to use when you need to select a type.
+    /// </summary>
+    public enum TypeSelectionStrategy
+    {
+        /// <summary>
+        /// Unknown (default).
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Use the declared type.
+        /// </summary>
+        UseDeclaredType,
+
+        /// <summary>
+        /// Use the runtime type.
+        /// </summary>
+        UseRuntimeType,
+    }
+}
