0e7c30a5063c12ec4c24a45c5135ddde21a30452
Suraj Gupta <SurajGupta@users.noreply.github.com>
2024-12-23 10:04:30 -0500

    added ThrowOpExecutionFailedExceptionOp/{TResult}

---
+++ OBeautifulCode.Type/Interfaces/Classification/Operation/Specified/IThrowOpExecutionFailedExceptionOp.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IThrowOpExecutionFailedExceptionOp.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.CodeAnalysis.Recipes;
+
+    /// <summary>
+    /// An operation that throws an <see cref="OpExecutionFailedExceptionBase"/>.
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = ObcSuppressBecause.CA1040_AvoidEmptyInterfaces_NeedToIdentifyGroupOfTypesAndPreferInterfaceOverAttribute)]
+    public interface IThrowOpExecutionFailedExceptionOp : IOperation, IHaveDetails
+    {
+    }
+}
