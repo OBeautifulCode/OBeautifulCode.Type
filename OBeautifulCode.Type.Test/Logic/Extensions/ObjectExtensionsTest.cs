e84af3972226421b88631137bd45894413e6c570
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-12-22 12:05:15 -0500

    added IObject.FindAllInPropertyGraph extension method

---
+++ OBeautifulCode.Type.Test/Logic/Extensions/ObjectExtensionsTest.cs
@@ -0,0 +1,256 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectExtensionsTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FakeItEasy;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.AutoFakeItEasy;
+    using Xunit;
+
+    public static class ObjectExtensionsTest
+    {
+        [Fact]
+        public static void FindAllInPropertyGraph___Should_throw_ArgumentNullException___When_parameter_rootObject_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ObjectExtensions.FindAllInPropertyGraph<IOperation>(null));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.AsTest().Must().ContainString("rootObject");
+        }
+
+        [Fact]
+        public static void FindAllInPropertyGraph___Should_return_rootObject___When_parameter_rootObject_is_of_type_TTarget()
+        {
+            // Arrange
+            var rootObject = new TestClass
+            {
+                Details = A.Dummy<string>(),
+            };
+
+            // Act
+            var actual = rootObject.FindAllInPropertyGraph<TestClass>();
+
+            // Assert
+            actual.AsTest().Must().HaveCount(1);
+            actual.Single().Details.AsTest().Must().BeEqualTo(rootObject.Details);
+        }
+
+        [Fact]
+        public static void FindAllInPropertyGraph___Should_return_rootObject_and_contained_object___When_parameter_rootObject_is_of_type_TTarget_and_contains_objects_of_type_TTarget()
+        {
+            // Arrange
+            var details = Some.ReadOnlyDummies<string>(3).ToList();
+
+            var rootObject = new TestClass
+            {
+                Details = details[0],
+                ListOfObjects = new List<IObject>
+                {
+                    new TestClass
+                    {
+                        Details = details[1],
+                    },
+                },
+                Object = new TestClass
+                {
+                    Details = details[2],
+                },
+            };
+
+            // Act
+            var actual = rootObject.FindAllInPropertyGraph<TestClass>();
+
+            // Assert
+            actual.AsTest().Must().HaveCount(3);
+            actual.Select(_ => _.Details).Must().BeUnorderedEqualTo(details);
+        }
+
+        [Fact]
+        public static void FindAllInPropertyGraph___Should_return_contained_objects___When_called()
+        {
+            // Arrange
+            var details = Some.ReadOnlyDummies<string>(9).ToList();
+
+            var rootObject = new TestClass
+            {
+                Details = details[0],
+                ListOfObjects = new List<IObject>
+                {
+                    new TestClass
+                    {
+                        Details = details[1],
+                        MapOfObjectToObject = new Dictionary<IObject, IObject[]>
+                        {
+                            {
+                                new TestClass
+                                {
+                                    Details = details[2],
+                                },
+                                new IObject[]
+                                {
+                                    new NullReturningOp<string>(),
+                                }
+                            },
+                        },
+                    },
+                    new TestClass
+                    {
+                        Details = details[3],
+                        ArrayOfObjects = new IObject[]
+                        {
+                            new SimpleValue<TestClass>(
+                                new TestClass
+                                {
+                                    Details = details[4],
+                                }),
+                            null,
+                        },
+                    },
+                    new SimpleValue<TestClass>(
+                        new TestClass
+                        {
+                            Details = details[5],
+                        }),
+                },
+                ArrayOfObjects = new IObject[]
+                {
+                    null,
+                    new SimpleValue<TestClass>(
+                        new TestClass
+                        {
+                            Details = details[6],
+                        }),
+                },
+                MapOfObjectToObject = new Dictionary<IObject, IObject[]>
+                {
+                    {
+                        new SimpleValue<SimpleValue<TestClass>>(
+                            new SimpleValue<TestClass>(
+                                new TestClass
+                                {
+                                    Details = details[7],
+                                })),
+                        new IObject[]
+                        {
+                            null,
+                        }
+                    },
+                },
+                Object = new TestClass
+                {
+                    Details = details[8],
+                },
+            };
+
+            // Act
+            var actual = rootObject.FindAllInPropertyGraph<TestClass>();
+
+            // Assert
+            actual.AsTest().Must().HaveCount(9);
+            actual.Select(_ => _.Details).Must().BeUnorderedEqualTo(details);
+        }
+
+        [Fact]
+        public static void FindAllInPropertyGraph___Should_not_return_same_object_multiple_times___When_object_graph_has_same_target_multiple_times()
+        {
+            // Arrange
+            var details = Some.ReadOnlyDummies<string>(7).ToList();
+
+            var sharedObject = new TestClass
+            {
+                Details = details[6],
+            };
+
+            var rootObject = new TestClass
+            {
+                Details = details[0],
+                ListOfObjects = new List<IObject>
+                {
+                    new TestClass
+                    {
+                        Details = details[1],
+                        MapOfObjectToObject = new Dictionary<IObject, IObject[]>
+                        {
+                            {
+                                sharedObject,
+                                new IObject[]
+                                {
+                                    new NullReturningOp<string>(),
+                                }
+                            },
+                        },
+                    },
+                    new TestClass
+                    {
+                        Details = details[2],
+                        ArrayOfObjects = new IObject[]
+                        {
+                            new SimpleValue<TestClass>(
+                                new TestClass
+                                {
+                                    Details = details[3],
+                                }),
+                            null,
+                        },
+                    },
+                    new SimpleValue<TestClass>(
+                        sharedObject),
+                },
+                ArrayOfObjects = new IObject[]
+                {
+                    null,
+                    new SimpleValue<TestClass>(
+                        new TestClass
+                        {
+                            Details = details[4],
+                        }),
+                },
+                MapOfObjectToObject = new Dictionary<IObject, IObject[]>
+                {
+                    {
+                        new SimpleValue<SimpleValue<TestClass>>(
+                            new SimpleValue<TestClass>(
+                                new TestClass
+                                {
+                                    Details = details[5],
+                                })),
+                        new IObject[]
+                        {
+                            null,
+                        }
+                    },
+                },
+                Object = sharedObject,
+            };
+
+            // Act
+            var actual = rootObject.FindAllInPropertyGraph<TestClass>();
+
+            // Assert
+            actual.AsTest().Must().HaveCount(7);
+            actual.Select(_ => _.Details).Must().BeUnorderedEqualTo(details);
+        }
+
+        private class TestClass : IObject
+        {
+            public string Details { get; set; }
+
+            public List<IObject> ListOfObjects { get; set; }
+
+            public IObject[] ArrayOfObjects { get; set; }
+
+            public IReadOnlyDictionary<IObject, IObject[]> MapOfObjectToObject { get; set; }
+
+            public IObject Object { get; set; }
+        }
+    }
+}
---
+++ OBeautifulCode.Type/Logic/Extensions/ObjectExtensions.cs
@@ -0,0 +1,158 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectExtensions.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Type
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using OBeautifulCode.Equality.Recipes;
+
+    /// <summary>
+    /// Extension methods on <see cref="IObject"/>.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Searches the full property graph of the specified <paramref name="rootObject"/>
+        /// and returns all objects whose runtime type is compatible (is or derives from)
+        /// the specified <typeparamref name="TTarget"/> type.
+        /// </summary>
+        /// <typeparam name="TTarget">The target type.</typeparam>
+        /// <param name="rootObject">The object whose property graph will be searched.</param>
+        /// <returns>
+        /// All objects compatible with <typeparamref name="TTarget"/> in the property graph of
+        /// <paramref name="rootObject"/>.
+        /// If <paramref name="rootObject"/> is itself compatible with <typeparamref name="TTarget"/>
+        /// then it will be returned, but also it's properties will be searched for objects compatible
+        /// with <typeparamref name="TTarget"/>.
+        /// The same object will not be returned multiple times, regardless of whether
+        /// it appears in the property graph multiple times.
+        /// </returns>
+        public static IReadOnlyCollection<TTarget> FindAllInPropertyGraph<TTarget>(
+            this IObject rootObject)
+            where TTarget : IObject
+        {
+            if (rootObject == null)
+            {
+                throw new ArgumentNullException(nameof(rootObject));
+            }
+
+            var result = rootObject.FindAllInPropertyGraphInternal<TTarget>().ToList();
+
+            return result;
+        }
+
+        private static IEnumerable<TTarget> FindAllInPropertyGraphInternal<TTarget>(
+            this IObject rootObject)
+            where TTarget : IObject
+        {
+            var visited = new HashSet<object>(new ReferenceEqualityComparer<object>());
+
+            foreach (var found in FindAllTargets(rootObject))
+            {
+                yield return found;
+            }
+
+            IEnumerable<TTarget> FindAllTargets(
+                object subject)
+            {
+                if (subject == null)
+                {
+                    yield break;
+                }
+
+                // Only track reference types for cycles.
+                // Also ensures that the same TTarget object isn't returned multiple times.
+                var subjectType = subject.GetType();
+                if (!subjectType.IsValueType)
+                {
+                    var subjectPreviouslyVisited = !visited.Add(subject);
+
+                    if (subjectPreviouslyVisited)
+                    {
+                        yield break;
+                    }
+                }
+
+                // If this node is (or derives from) the target type, return it.
+                if (subject is TTarget target)
+                {
+                    yield return target;
+                }
+
+                // If it's a collection, traverse its items.
+                if (subject is IEnumerable enumerableSubject)
+                {
+                    foreach (var element in enumerableSubject)
+                    {
+                        foreach (var found in FindAllTargets(element))
+                        {
+                            yield return found;
+                        }
+                    }
+
+                    yield break;
+                }
+
+                // Traverse readable, non-indexer instance properties
+                var propertiesToSearch = subjectType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                foreach (var property in propertiesToSearch)
+                {
+                    if (!property.CanRead)
+                    {
+                        continue;
+                    }
+
+                    if (property.GetIndexParameters().Length != 0)
+                    {
+                        continue;
+                    }
+
+                    var propertyType = property.PropertyType;
+
+                    // Prune obvious leaves
+                    if (propertyType.IsPrimitive || propertyType.IsEnum)
+                    {
+                        continue;
+                    }
+
+                    // Common types whose properties don't need to be searched.
+                    if ((propertyType == typeof(string)) ||
+                        (propertyType == typeof(decimal)) ||
+                        (propertyType == typeof(DateTime)) ||
+                        (propertyType == typeof(DateTimeOffset)) ||
+                        (propertyType == typeof(TimeSpan)) ||
+                        (propertyType == typeof(Guid)) ||
+                        (propertyType == typeof(Version)) ||
+                        (propertyType == typeof(Uri)))
+                    {
+                        continue;
+                    }
+
+                    object propertyValue;
+                    try
+                    {
+                        // This can execute code.
+                        propertyValue = property.GetValue(subject, null);
+                    }
+                    catch
+                    {
+                        // Ignore properties whose getters throw
+                        continue;
+                    }
+
+                    foreach (var found in FindAllTargets(propertyValue))
+                    {
+                        yield return found;
+                    }
+                }
+            }
+        }
+    }
+}
